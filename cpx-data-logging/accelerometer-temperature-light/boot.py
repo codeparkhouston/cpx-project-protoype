03866cf336afd9c49922b35258849b0d6b911ff0
amanda <pandafulmanda@gmail.com>
2018-08-06 07:46:22 -0500

    spelling

--- cpx-data-logging/accelerometer-temperature-light/boot.py
+++ cpx-data-logging/accelerometer-temperature-light/boot.py
@@ -7,7 +7,7 @@ from adafruit_circuitplayground.express import cpx
 # 
 # Our cpx board is usually like a USB drive --
 # we can write information or code to it over USB.
-# To save information that cpx senses to iself,
+# To save information that cpx senses to itself,
 # the storage mode will need to be set to data logging mode.
 import storage
 
